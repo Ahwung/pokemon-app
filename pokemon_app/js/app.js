733a05f5ae32b97a193fc6ea8701a9ffe5d2fab6
Adam Hwung <Ahwung@gmail.com>
2019-10-18 13:48:20 -0700

    added click handlers for buttons to cycle through image carousel

--- pokemon_app/js/app.js
+++ pokemon_app/js/app.js
@@ -1,6 +1,32 @@
 
 
+
+
 $(() => {
+
+	let currentImgIndex = 0;
+	let highestImgIndex = $('.carousel-images').children().length - 1
+
+	$('.next').on('click', () => {
+		$('.carousel-images').children().eq(currentImgIndex).css('display', 'none')
+		if (currentImgIndex < highestImgIndex) {
+			currentImgIndex++;
+		} else {
+			currentImgIndex = 0
+		}
+		$('.carousel-images').children().eq(currentImgIndex).css('display', 'block')
+	})
+
+	$('.previous').on('click', () => {
+		$('.carousel-images').children().eq(currentImgIndex).css('display', 'none')
+		if (currentImgIndex > 0) {
+			currentImgIndex--;
+		} else {
+			currentImgIndex = highestImgIndex
+		}
+		$('.carousel-images').children().eq(currentImgIndex).css('display', 'block')
+	})
+
 
 	$.ajax({
 		url: "https://pokeapi.co/api/v2/pokemon/1",
