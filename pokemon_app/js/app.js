54ef4a358f5ba3ab5040a0e332e22c52e61bdfaf
Adam Hwung <Ahwung@gmail.com>
2019-10-19 10:55:44 -0700

    added html for modal div

--- pokemon_app/js/app.js
+++ pokemon_app/js/app.js
@@ -4,9 +4,12 @@
 
 $(() => {
 
+
+	// Variables for next and previous buttons
 	let currentImgIndex = 0;
 	let highestImgIndex = $('.carousel-images').children().length - 1
 
+	// Click event for next button
 	$('.next').on('click', () => {
 		$('.carousel-images').children().eq(currentImgIndex).css('display', 'none')
 		if (currentImgIndex < highestImgIndex) {
@@ -17,6 +20,7 @@ $(() => {
 		$('.carousel-images').children().eq(currentImgIndex).css('display', 'block')
 	})
 
+	// Click event for previous button
 	$('.previous').on('click', () => {
 		$('.carousel-images').children().eq(currentImgIndex).css('display', 'none')
 		if (currentImgIndex > 0) {
@@ -27,7 +31,7 @@ $(() => {
 		$('.carousel-images').children().eq(currentImgIndex).css('display', 'block')
 	})
 
-
+	// Ajax call to get data from PokeAPI
 	$.ajax({
 		url: "https://pokeapi.co/api/v2/pokemon/1",
 		type: "GET",
