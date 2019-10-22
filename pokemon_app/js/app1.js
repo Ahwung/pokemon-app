5c911679f231b41bacee64bb4e9ee55fd1b28ff6
Adam Hwung <Ahwung@gmail.com>
2019-10-22 09:40:00 -0700

    added pictures for database page

--- pokemon_app/js/app1.js
+++ pokemon_app/js/app1.js
@@ -1,14 +1,15 @@
 
 $(() => {
 
-	for (let i = 1; i <= 3; i++) {
+	for (let i = 1; i <= 151; i++) {
 		$.ajax({
 			url: "https://pokeapi.co/api/v2/pokemon/" + i,
 			type: "GET",
 		}).then(
 		(data) => {
-			console.log(data.sprites.front_default)
-			const $img = $()
+			// console.log(data.sprites.front_default)
+			const $img = $('<img>').attr('src', data.sprites.front_default).attr('alt', data.name)
+			$('.gen-1').append($img)
 		})
 	}
 	
