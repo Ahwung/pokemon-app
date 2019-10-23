1095928da43a874b878c80b4548ccbf23dfead6c
Adam Hwung <Ahwung@gmail.com>
2019-10-22 19:50:52 -0700

    added more pokemon to database page and added show/hide functionality

--- pokemon_app/js/database_app.js
+++ pokemon_app/js/database_app.js
@@ -2,12 +2,38 @@
 
 $(() => {
 
-	const getData = () => {
+	let cover1 = true;
+	let cover2 = true;
+	let cover3 = true;
 
-	}
+	$('.gen-1').on('click', (event) => {
+		if (cover1 === true) {
+			$('.gen-1-sprites').hide()
+			cover1 = false
+		} else if (cover1 === false) {
+			$('.gen-1-sprites').show()
+			cover1 = true;
+		}
+	})
 
-	$('.close').on('click', () => {
-		$('.modal').hide();
+	$('.gen-2').on('click', (event) => {
+		if (cover2 === true) {
+			$('.gen-2-sprites').hide()
+			cover2 = false
+		} else if (cover2 === false) {
+			$('.gen-2-sprites').show()
+			cover2 = true;
+		}
+	})
+
+	$('.gen-3').on('click', (event) => {
+		if (cover3 === true) {
+			$('.gen-3-sprites').hide()
+			cover3 = false
+		} else if (cover3 === false) {
+			$('.gen-3-sprites').show()
+			cover3 = true;
+		}
 	})
 
 	for (let i = 1; i <= 151; i++) {
@@ -28,7 +54,40 @@ $(() => {
 		})
 	}
 
+	for (let i = 152; i <= 251; i++) {
+		$.ajax({
+			url: "https://pokeapi.co/api/v2/pokemon/" + i,
+			type: "GET",
+		}).then(
+		(data) => {
+			// console.log(data.sprites.front_default)
+			const $img = $('<img>').attr('src', data.sprites.front_default).attr('alt', data.name).attr('id', data.id).addClass('pokemonImage')
+			$('.gen-2-sprites').append($img)
 
+
+		}).then(() => {
+			$('.pokemonImage').on('click', (event) => {
+				console.log(event.target)
+			})
+		})
+	}
 	
+	for (let i = 252; i <= 386; i++) {
+		$.ajax({
+			url: "https://pokeapi.co/api/v2/pokemon/" + i,
+			type: "GET",
+		}).then(
+		(data) => {
+			// console.log(data.sprites.front_default)
+			const $img = $('<img>').attr('src', data.sprites.front_default).attr('alt', data.name).attr('id', data.id).addClass('pokemonImage')
+			$('.gen-3-sprites').append($img)
+
+
+		}).then(() => {
+			$('.pokemonImage').on('click', (event) => {
+				console.log(event.target)
+			})
+		})
+	}
 	
 });
